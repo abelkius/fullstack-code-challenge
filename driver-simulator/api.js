75ca75ece4180bb0df466381bdb5939b65a346d2
abelkius <abelkius@gmail.com>
2017-11-05 23:37:42 +0100

    Revert "changed the variable name to https for consistency" because of wrong formatting
    This reverts commit 42135a3cd2cae5a7dd88a1d32a55286aaff833aa.

--- driver-simulator/api.js
+++ driver-simulator/api.js
@@ -1,38 +1,44 @@
-const https = require('https');
+const http = require("https");
 
 const API_HOST = process.argv[2];
 const API_PREFIX = process.argv[3];
 
 function updateVehicleLocation(vehicleId, lat, lng) {
-  request(`/vehicles/${vehicleId}/locations`, 'POST', {lat, lng, at: new Date().toISOString()}, res => {
-    console.log(`Vehicle ${vehicleId.split('-')[0]} moved to: ${lat}, ${lng}`);
-  });
+  request(
+    `/vehicles/${vehicleId}/locations`,
+    "POST",
+    { lat, lng, at: new Date().toISOString() },
+    res => {
+      console.log(
+        `Vehicle ${vehicleId.split("-")[0]} moved to: ${lat}, ${lng}`
+      );
+    }
+  );
 }
 
 function registerVehicle(vehicleId) {
-  request(`/vehicles`, 'POST', {id: vehicleId}, res => {
+  request(`/vehicles`, "POST", { id: vehicleId }, res => {
     console.log(`New vehicle registered: ${vehicleId}`);
   });
 }
 
 function deregisterVehicle(vehicleId) {
-  request(`/vehicles/${vehicleId}`, 'DELETE', null, res => {
+  request(`/vehicles/${vehicleId}`, "DELETE", null, res => {
     console.log(`Vehicle de-registered: ${vehicleId}`);
   });
 }
 
 function request(path, method, body, cb) {
-  const data = JSON.stringify(body);
+  const data = JSON.stringify(body)
   path = '/' + API_PREFIX + path;
 
-  const req = https.request(
-    {
-      hostname: API_HOST,
+  const req = http.request(
+    { hostname: API_HOST,
       path: path,
       method: method,
       headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': Buffer.byteLength(data)
+        "Content-Type": "application/json",
+        "Content-Length": Buffer.byteLength(data)
       }
     },
     cb
@@ -42,9 +48,11 @@ function request(path, method, body, cb) {
     req.write(data);
   }
 
-  req.on('error', error => {
-    if (error.code === 'ECONNREFUSED') {
-      console.log(`The simulator couldn't reach the API at ${API_HOST}:${API_PORT}`);
+  req.on("error", error => {
+    if (error.code === "ECONNREFUSED") {
+      console.log(
+        `The simulator couldn't reach the API at ${API_HOST}:${API_PORT}`
+      );
     } else {
       console.log(error);
     }
